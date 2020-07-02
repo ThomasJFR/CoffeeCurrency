73eed9e14ae6b58781d134ebde5f2c74f86bb3c7
Thomas <thomas.joakim@gmail.com>
2020-07-02 08:23:25 +0200

    Initial commit of files

---
+++ content.js
@@ -0,0 +1,19 @@
+//var cheeks = jQuery.noConflict(true);
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+			var replacedText = text.replace(/\$/g || "$" || "$", 'chubby');
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
