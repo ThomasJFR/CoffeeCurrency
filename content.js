29de0d17b1fd2e50625d051c7195df8a600da039
Luke Chen <67808339+LukeChenTzu@users.noreply.github.com>
2020-07-06 08:25:16 -0700

    Update content.js

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-			var replacedText = text.replace(/\$/g || "$" || "$", 'chubby');
+			var replacedText = text.replace(/\$/g || "$", 'chubby');
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
